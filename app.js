edcc869ef5ea34aaf41e730c7274ba4b950d9f1d
ayma2410 <aymashahid2010@gmail.com>
2026-07-29 23:42:42 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,209 +1,70 @@
+let student = [];
+function showMessage(){
+        alert ("welcome to studyhub dashbord!");
+    }
 
+let students = [];
 
-// // Question 1
-// var studentNames = [];
+function addStudent() {
 
-// // Question 2
-// var studentNames = new Array();
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let course = document.getElementById("course").value;
 
-// // Question 3
-// var fruits = ["Apple", "Banana", "Mango", "Orange"];
+    let student = {
+        name: name,
+        age: age,
+        course: course
+    };
 
-// // Question 4
-// var numbers = [10, 20, 30, 40, 50];
+    students.push(student);
 
-// // Question 5
-// var booleanArray = [true, false, true, false];
+    displayStudents();
 
-// // Question 6
+    clearForm();
+}
 
-// var mixedArray = ["Ali", 20, true, 45.5];
-// document.write(mixedArray + "<br><br>");
 
-// // Question 7
+function displayStudents() {
 
-// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+    let container = document.getElementById("studentContainer");
 
-// document.write("<h3>Qualifications:</h3>");
+    container.innerHTML = "";
 
-// for (var i = 0; i < qualifications.length; i++) {
-//     document.write((i + 1) + ") " + qualifications[i] + "<br>");
-// }
+    for(let i = 0; i < students.length; i++) {
 
-// // Question 8
+        container.innerHTML += `
+        <div>
+            <h3>${students[i].name}</h3>
+            <p>Age: ${students[i].age}</p>
+            <p>Course: ${students[i].course}</p>
 
-// var students = ["Ali", "Ahmed", "Sara"];
-// var scores = [400, 450, 380];
+            <button onclick="deleteStudent(${i})">
+            Delete
+            </button>
 
-// for (var i = 0; i < students.length; i++) {
-//     var percentage = (scores[i] / 500) * 100;
-//     document.write("Score of " + students[i] + " is " + scores[i] +
-//     ". Percentage: " + percentage + "%<br>");
-// }
+        </div>
+        `;
+    }
 
-// // Question 9
+    document.getElementById("totalStudents").innerHTML =
+    "Total Students: " + students.length;
+}
 
-// var colors = ["Red", "Green", "Blue"];
 
-// document.write(colors + "<br>");
+function clearForm(){
 
-// var color1 = prompt("Enter color to add at beginning:");
-// colors.unshift(color1);
-// document.write(colors + "<br>");
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("course").value = "";
 
-// var color2 = prompt("Enter color to add at end:");
-// colors.push(color2);
-// document.write(colors + "<br>");
+}
 
-// colors.unshift("Black", "White");
-// document.write(colors + "<br>");
 
-// colors.shift();
-// document.write(colors + "<br>");
+function deleteStudent(index){
 
-// colors.pop();
-// document.write(colors + "<br>");
+    students.splice(index,1);
 
-// var index = +prompt("Enter index to add color:");
-// var newColor = prompt("Enter color name:");
-// colors.splice(index, 0, newColor);
-// document.write(colors + "<br>");
+    displayStudents();
 
-// var delIndex = +prompt("Enter index to delete:");
-// var delCount = +prompt("How many colors to delete?");
-// colors.splice(delIndex, delCount);
-// document.write(colors + "<br>");
-
-// // Question 10
-
-// var scores = [320, 230, 480, 120];
-// document.write("Scores: " + scores + "<br>");
-
-// scores.sort(function(a, b) {
-//     return a - b;
-// });
-
-// document.write("Ordered Scores: " + scores);
-
-// // Question 11
-
-// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
-// var selectedCities = cities.slice(1, 4);
-
-// document.write("Cities: " + cities + "<br>");
-// document.write("Selected Cities: " + selectedCities);
-
-// // Question 12
-
-// var arr = ["This", "is", "my", "cat"];
-// var result = arr.join(" ");
-
-// document.write(result);
-
-// // Question 13
-
-// var devices = [];
-
-// devices.push("Keyboard");
-// devices.push("Mouse");
-// devices.push("Printer");
-// devices.push("Monitor");
-
-// document.write(devices.shift() + "<br>");
-// document.write(devices.shift() + "<br>");
-// document.write(devices.shift() + "<br>");
-// document.write(devices.shift() + "<br>");
-
-// // Question 14
-
-// var devices = [];
-
-// devices.push("Keyboard");
-// devices.push("Mouse");
-// devices.push("Printer");
-// devices.push("Monitor");
-
-// document.write(devices.pop() + "<br>");
-// document.write(devices.pop() + "<br>");
-// document.write(devices.pop() + "<br>");
-// document.write(devices.pop() + "<br>");
-
- question1
- // Question 1
-
-// var arr = [];
-
-// console.log(arr);
-
-// // Question 2
-
-// var matrix = [
-//     [0, 1, 2, 3],
-//     [1, 0, 1, 2],
-//     [2, 1, 0, 1]
-// ];
-
-// for (var i = 0; i < matrix.length; i++) {
-//     console.log(matrix[i].join(" "));
-// }
-
-// // Question 3
-
-// for (var i = 1; i <= 10; i++) {
-//     console.log(i);
-// }
-
-// // Question 4
-
-// var table = +prompt("Enter table number:");
-// var length = +prompt("Enter table length:");
-
-// for (var i = 1; i <= length; i++) {
-//     document.write(table + " x " + i + " = " + (table * i) + "<br>");
-// }
-// // Question 5
-
-// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
-
-// for (var i = 0; i < fruits.length; i++) {
-//     document.write(fruits[i] + "<br>");
-// }
-
-// document.write("<br>");
-
-// for (var i = 0; i < fruits.length; i++) {
-//     document.write("Element at index " + i + " is " + fruits[i] + "<br>");
-// }
-
-// // Question 6
-
-// document.write("<b>Counting:</b><br>");
-
-// for (var i = 1; i <= 15; i++) {
-//     document.write(i + ", ");
-// }
-
-// document.write("<br><br><b>Reverse Counting:</b><br>");
-
-// for (var i = 10; i >= 1; i--) {
-//     document.write(i + ", ");
-// }
-
-// document.write("<br><br><b>Even:</b><br>");
-
-// for (var i = 0; i <= 20; i += 2) {
-//     document.write(i + ", ");
-// }
-
-// document.write("<br><br><b>Odd:</b><br>");
-
-// for (var i = 1; i <= 19; i += 2) {
-//     document.write(i + ", ");
-// }
-
-// document.write("<br><br><b>Series:</b><br>");
-
-// for (var i = 2; i <= 20; i += 2) {
-//     document.write(i + "k, ");
-// }
-
+}
